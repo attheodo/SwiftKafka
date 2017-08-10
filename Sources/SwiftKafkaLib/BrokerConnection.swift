65dad04ced4595cb265f067081fb9e24a2374f54
attheodo <at@atworks.gr>
2017-08-11 02:07:23 +0300

    struct for controlling broker connection objects

---
+++ Sources/SwiftKafkaLib/BrokerConnection.swift
@@ -0,0 +1,41 @@
+/// An enum representing the protocol used for a broker connection
+public enum BrokerConnectionProtocol: String {
+    
+    case plaintext = "PLAINTEXT://"
+    case ssl = "SSL://"
+    case sasl = "SASL://"
+    case sasl_plaintext = "SASL_PLAINTEXT://"
+    
+}
+
+public struct BrokerConnection {
+    
+    // MARK: - Private Properties
+    
+    /// The used protocol for this broker connection
+    fileprivate let `protocol`: BrokerConnectionProtocol
+    
+    /// The port of this broker connection
+    fileprivate let port: Int
+    
+    /// The host of this broker connection
+    fileprivate let host: String
+    
+    // MARK: - Initialiser
+    init(withProtocol protocol: BrokerConnectionProtocol, host: String, port: Int) {
+        
+        self.`protocol` = `protocol`
+        self.host = host
+        self.port = port
+        
+    }
+    
+}
+
+extension BrokerConnection: CustomStringConvertible {
+    
+    public var description: String {
+        return "\(`protocol`)\(host):\(port)"
+    }
+    
+}
