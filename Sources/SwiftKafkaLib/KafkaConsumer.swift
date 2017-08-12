c7cab4846b4686a11f0a48aef6a24a45b1922d86
attheodo <at@atworks.gr>
2017-08-12 17:15:55 +0300

    namin is hard

---
+++ Sources/SwiftKafkaLib/KafkaConsumer.swift
@@ -0,0 +1,5 @@
+import ckafka
+
+class KafkaClient: SwiftKafka {
+    
+}
---
+++ Sources/SwiftKafkaLib/TopicConfig.swift
@@ -0,0 +1,9 @@
+//
+//  TopicConfig.swift
+//  SwiftKafka
+//
+//  Created by Athanasios Theodoridis on 11/08/2017.
+//
+//
+
+import Foundation
