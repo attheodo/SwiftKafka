e07ec6516b317cec197b5f3dc6d748bb9851951a
attheodo <at@atworks.gr>
2017-08-12 17:16:12 +0300

    struct for dealing with topic configs

--- Sources/SwiftKafkaLib/TopicConfig.swift
+++ Sources/SwiftKafkaLib/TopicConfig.swift
@@ -1,9 +1,118 @@
-//
-//  TopicConfig.swift
-//  SwiftKafka
-//
-//  Created by Athanasios Theodoridis on 11/08/2017.
-//
-//
+import ckafka
 
-import Foundation
+public struct TopicConfig {
+    
+    // MARK: - Public Properties
+    public var properties: [String: String]? {
+        return listConfigurationProperties()
+    }
+    
+    public private(set) var configHandle: OpaquePointer
+    
+    // MARK: - Initialiser
+    init(byDuplicatingConfig config: TopicConfig? = nil) throws {
+        
+        if let config = config {
+            
+            guard let cfg = rd_kafka_topic_conf_dup(config.configHandle) else {
+                throw KafkaError.topicConfigDuplicationError
+            }
+            
+            configHandle = cfg
+            
+        } else {
+            
+            guard let cfg = rd_kafka_topic_conf_new() else {
+                throw KafkaError.topicConfigCreationError
+            }
+            
+            configHandle = cfg
+            
+        }
+        
+    }
+    
+    // MARK: - Public Methods
+    
+    /**
+     Returns the value of a topic configuration variable
+     - parameter variable: The name of the variable for which to get the value
+     - returns: A `Srting` containing the value of the topic configuration variable
+     */
+    public func get(_ variable: String) throws -> String {
+        
+        guard let value = properties?[variable] else {
+            throw KafkaError.configVariableNotFound(variable)
+        }
+        
+        return value
+        
+    }
+    
+    /**
+     Sets a value to a topic configuration variable
+     - parameter variable: The name of the variable to set the value for
+     - parameter value: The value of the configuration variable to set
+     */
+    public func set(_ variable: String, value: String) throws {
+        
+        let errStr = UnsafeMutablePointer<Int8>.allocate(capacity: kSwiftKafkaCStringSize)
+        let result = rd_kafka_topic_conf_set(configHandle, variable, value, errStr, kSwiftKafkaCStringSize)
+        
+        defer {
+            errStr.deallocate(capacity: kSwiftKafkaCStringSize)
+        }
+        
+        guard result == RD_KAFKA_CONF_OK else {
+            
+            let errorDesc = String(cString: errStr)
+            
+            if result == RD_KAFKA_CONF_INVALID {
+                throw KafkaError.setConfigurationPropertyError(errorDesc)
+            }
+            
+            if result == RD_KAFKA_CONF_UNKNOWN {
+                throw KafkaError.setConfigurationPropertyError(errorDesc)
+            }
+            
+            return
+            
+        }
+    }
+    
+    // MARK: - Private Methods
+    
+    /**
+     Returns a dictionary containing all the properties
+     and values of the current configuration
+     */
+    private func listConfigurationProperties() -> [String: String]? {
+        
+        var properties: [String: String] = [:]
+        var propertiesCount = 0
+        
+        guard let d = rd_kafka_topic_conf_dump(configHandle, &propertiesCount), propertiesCount > 0 else {
+            return nil
+        }
+        
+        defer {
+            rd_kafka_conf_dump_free(d, propertiesCount)
+        }
+        
+        for i in 0...propertiesCount / 2 {
+            
+            guard let key = d.advanced(by: i*2).pointee,
+                let value = d.advanced(by: i*2 + 1).pointee else
+            {
+                break
+            }
+            
+            properties[String(cString: key)] = String(cString: value)
+            
+        }
+        
+        return properties
+        
+    }
+
+}
