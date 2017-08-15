1527a4bb9aa08120a78f2ad4a5bf4ae1e6544999
attheodo <at@atworks.gr>
2017-08-15 18:23:22 +0300

    struct and helper methods for dealing with kafka messages

--- Sources/SwiftKafkaLib/KafkaMessage.swift
+++ Sources/SwiftKafkaLib/KafkaMessage.swift
@@ -1,17 +1,47 @@
+import Foundation
+
 import ckafka
 
 public struct KafkaMessage {
     
     // MARK: - Static
-    public static func message(fromRawMessage message: rd_kafka_message_t) -> KafkaMessage {
+    public static func message(fromRawMessage message: rd_kafka_message_t,
+                               topic: String? = nil) -> KafkaMessage
+    {
         
         var error: KafkaError? = nil
+        var key: Data? = nil
+        var value: Data? = nil
         
+        // set the key if any
+        if let keyPointer = message.key {
+            
+            let p = keyPointer.assumingMemoryBound(to: Int8.self)
+            var keyData = Array(UnsafeBufferPointer<Int8>(start: p, count: message.key_len))
+            
+            key = Data(bytes: &keyData, count: keyData.count)
+            
+        }
+        
+        // set the value if any
+        if let valuePointer = message.payload {
+            
+            let p = valuePointer.assumingMemoryBound(to: Int8.self)
+            var valueData = Array(UnsafeBufferPointer<Int8>(start: p, count: message.len))
+            
+            value = Data(bytes: &valueData, count: valueData.count)
+            
+        }
+        
+        // set the error if any
         if message.err != RD_KAFKA_RESP_ERR_NO_ERROR {
             error = KafkaError.coreError(KafkaCoreError(rdError: message.err))
         }
         
-        return KafkaMessage(partition: message.partition,
+        return KafkaMessage(topic: topic,
+                            key: key,
+                            value: value,
+                            partition: message.partition,
                             offset: message.offset,
                             error: error)
         
@@ -19,13 +49,105 @@ public struct KafkaMessage {
     
     // MARK: - Public Properties
     
+    /// The topic for this message
+    public let topic: String?
+    
     /// The partition of the message
-    public var partition: Int32 = 0
+    public let partition: Int32
     
     /// The offset of the message inside the topic
-    public var offset: Int64 = -1
+    public let offset: Int64
     
     /// Whether the message has an error or not
-    public var error: KafkaError? = nil
+    public let error: KafkaError?
+    
+    /// The message key
+    public let key: Data?
+    
+    /// The message value
+    public let value: Data?
+    
+    /// The value of `key` as a String, if convertible
+    public var keyString: String? {
+        
+        guard let key = self.key else {
+            return nil
+        }
+        
+        if let string = String(data: key, encoding: .utf8) {
+            return string
+        } else {
+            return nil
+        }
+        
+    }
+    
+    /// The value of `value` as a String, if convertible
+    public var valueString: String? {
+        
+        guard let value = self.value else {
+            return nil
+        }
+        
+        if let string = String(data: value, encoding: .utf8) {
+            return string
+        } else {
+            return nil
+        }
+        
+    }
+
+    
+    // MARK: - Initialiser
+    init(topic: String?, key: Data?, value: Data?, partition: Int32, offset: Int64, error: KafkaError?) {
+        
+        self.topic = topic
+        self.key = key
+        self.value = value
+        self.partition = partition
+        self.offset = offset
+        self.error = error
+        
+    }
+    
+}
+
+extension KafkaMessage: CustomStringConvertible {
+    
+    public var description: String {
+        
+        if let key = self.key, let value = self.value {
+            
+            if let keyString = self.keyString {
+                
+                if let valueString = self.valueString {
+                    return "#\(offset) [\(topic ?? "UKNOWN_TOPIC")] - [\(keyString)]=\"\(valueString)\""
+                } else {
+                    return "#\(offset) [\(topic ?? "UKNOWN_TOPIC")] - [\(keyString)]=<data>(\(value.count))"
+                }
+                
+            } else {
+                
+                if let valueString = self.valueString {
+                    return "#\(offset) [\(topic ?? "UKNOWN_TOPIC")] - [<data>(\(key.count))]=\"\(valueString)\""
+                } else {
+                    return "#\(offset) [\(topic ?? "UKNOWN_TOPIC")] - [<data>(\(key.count))]=<data>(\(value.count))"
+                }
+
+            }
+            
+        } else if let value = self.value {
+            
+            if let valueString = self.valueString {
+                return "#\(offset) [\(topic ?? "UKNOWN_TOPIC")] - [NO_KEY]=\"\(valueString)\""
+            } else {
+                return "#\(offset) [\(topic ?? "UKNOWN_TOPIC")] - [NO_KEY]=<data>(\(value.count))"
+            }
+
+        } else {
+            return "#\(offset) [\(topic ?? "UKNOWN_TOPIC")] - [NO_KEY]=\"(NO_VALUE)\""
+        }
+        
+    }
     
 }
